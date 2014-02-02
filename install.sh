26e71566f68ac496e8478b9f6dd42a73c13ec102
Aki <aki.onetime@gmail.com>
2014-02-03 00:28:30 +0900

    init

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# make symbolic links
+
+link_file() {
+  source="${PWD}/$1"
+  target="${HOME}/${1/_/.}"
+
+  if [ -e $target ] ; then
+    if [ ! -d $target ] ; then
+      echo "Update\t$target"
+      mv $target $target.bak
+      ln -sf ${source} ${target}
+    fi
+  else
+    echo "Install\t$target"
+    ln -sf ${source} ${target}
+  fi
+}
+
+for i in _*
+do
+  link_file $i
+done
+link_file bin
+
